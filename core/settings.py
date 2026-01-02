305f0e415e20c54aecbcfef6a2d98ac963f21cb9
Temurkan <famill1949@gmail.com>
2026-01-02 20:23:25 +0500

    Fix MEDIA_ROOT for Render

--- core/settings.py
+++ core/settings.py
@@ -166,7 +166,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = config.MEDIA_ROOT
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
